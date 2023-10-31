68423e2f79062f6d0fbd0573dcfe255bc3f0acea
ylug <amaterasumyeye@gmail.com>
2023-10-31 23:30:06 +0300

    изменения

---
+++ src/keyboard.py
@@ -0,0 +1,25 @@
+from src.item import Item
+
+
+class MixinLang:
+    def __init__(self):
+        self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+        return self
+
+
+class Keyboard(Item, MixinLang):
+
+    def __init__(self, name, price, quantity, language="EN"):
+        super().__init__(name, price, quantity)
+        self.__language = language
+        MixinLang.__init__(self)
